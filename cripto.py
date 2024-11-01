3be2aad4d40d6509b8ec18d69b92fa4487ce92ba
Guilherme Machado <157429638+Guilherme-axt@users.noreply.github.com>
2024-11-01 15:23:14 -0300

    cripto.py

--- cripto.py
+++ cripto.py
@@ -1,4 +1,4 @@
-frase = str(input("Digite sua palavra: "))
+palavra = str(input("Digite sua palavra: "))
 
 def letra_para_numero(palavra, separador='-'):
     resultado = []
@@ -7,8 +7,5 @@ def letra_para_numero(palavra, separador='-'):
             numero = ord(letra.lower()) - ord('a') + 1  # Calcula a posição
             resultado.append(str(numero))
     return separador.join(resultado)
-
-
-palavra = frase
 resultado = letra_para_numero(palavra)
 print("Sua palavra criptografada é: {}".format(resultado))  # Saída
